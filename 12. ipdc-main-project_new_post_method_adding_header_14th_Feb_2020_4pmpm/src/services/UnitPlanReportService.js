94be73a7a73a8a8b9a7e7b6ebe4f085e9d8231d7
frsummit <www.frsummit1@gmail.com>
2020-02-14 16:10:41 +0600

    this is the 11th project's carbon copy, post method setup for post request

---
+++ 12. ipdc-main-project_new_post_method_adding_header_14th_Feb_2020_4pmpm/src/services/UnitPlanReportService.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:50009/reporting/v1/all';
+
+const token = 'Bearer ' + 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJEUkNSVVkwUkVaQ1JUWTJSREl6TlRkQk1VUTVPVEkyTkRCQ1JqZzBSRU0zUmpBMFF6SkNRUSJ9.eyJodHRwczovL3JlcG9ydGluZ21vZHVsZS5jb20vY2xhaW1zL0lzU3lzdGVtQWRtaW4iOnRydWUsImh0dHBzOi8vcmVwb3J0aW5nbW9kdWxlLmNvbS9jbGFpbXMvSXNTeXN0ZW1Vc2VyIjp0cnVlLCJodHRwczovL3JlcG9ydGluZ21vZHVsZS5jb20vY2xhaW1zL3VzZXJuYW1lIjoicml6d2FudWxAc2ltcGxleGh1Yi5jb20iLCJpc3MiOiJodHRwczovL2Rldi1udXpmcTU2My5hdS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NWQyZWIwYjY5NDk1MjYwZWJjNWQ2YTVmIiwiYXVkIjpbImh0dHA6Ly8zNS4yMDEuMC4zOS8iLCJodHRwczovL2Rldi1udXpmcTU2My5hdS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNTgxNTczMjk3LCJleHAiOjE1ODE1ODA0OTcsImF6cCI6IjhWbmdrVmI1MWdra0Jadml0U29yZEJONGw4cklPd2V0Iiwic2NvcGUiOiJvcGVuaWQgZW1haWwifQ.1Nun4xOo3cvVkhgb_Fk2hHwPlQancA7abBMqdHXxYm_u4Pfgg5NC34U8cGLFYw4ElSzJILz8bRB2oYdn0bHWdxhDJeXXDGSZ0jN4dECAot8EanNKr-VKJDwwa7ABP_iVdgOh7E2UOtrbWyglAmG2tzTB_QJwZSnQNRAaGhj1gB99MLyFEeZQgxZnrcL8MxzpC44PIZM30GgzHk3By3YfKxnS8JdJ81t5fs_iDPd_hX4bkO2lGUBleBr888hi5Z0TI7oGduEjL71lRSy9KD6drPZaFBxKh3v3r7-1yPZMtGS3lSSlHdUtqfjbSNPaMUOhsqlGGvQdEQ_NVNMGw4tMrg'
+
+const BASE_URL = "http://localhost:50009/reporting/v1";
+const URL = "http://localhost:50009/reporting/v1/all/search?page=1&pageSize=10";
+
+const URL_SEARCH_PAGE = "/all/search?page=1&pageSize=10";
+const URL_SEARCH_ORGANIZATION = "/organization/search?page=1&pageSize=10"
+const URL_ORGANIZATION = "/organization/myorganizations";
+const URL_ORGANIZATION_USER = "/organization/user/search?page=1&pageSize=10";
+const URL_PLAN = '/unit/plan/10'
+
+export default class PostService{
+    getAllPosts() {
+        return axios.get(URL, {
+            headers: {
+                   Authorization: token 
+               } 
+        });
+    }
+    getSearchAllPage() {
+        return axios.get(BASE_URL + URL_SEARCH_PAGE, {
+            headers: {
+                   Authorization: token 
+               } 
+        });
+    }
+    getSearchOrganization() {
+        return axios.get(BASE_URL + URL_SEARCH_ORGANIZATION, {
+            headers: {
+                   Authorization: token 
+               } 
+        });
+    }
+    getOrganization() {
+        return axios.get(BASE_URL + URL_ORGANIZATION, {
+            headers: {
+                   Authorization: token 
+               } 
+        });
+    }
+    getOrganizationUser() {
+        return axios(BASE_URL + URL_ORGANIZATION_USER, {
+            headers: {
+                   Authorization: token 
+               },
+            method: 'get'
+        });
+    }
+    getPlanById() {
+        return axios(BASE_URL + URL_PLAN, {
+            headers: {
+                   Authorization: token 
+               },
+            method: 'get'
+        });
+    }
+    createPlan2(link, datas) {
+        const headers = new Headers();
+        headers.set("Authorization", token);
+        headers.set("SignalRConnectionId", '8ce05f33-570c-4b0c-9abd-b2252d3fcc08');
+        headers.set("Accept", "application/json");
+        headers.set("Content-Type", "application/json");
+        console.log(link);
+        console.log(datas.id);
+
+        return axios(link, {
+            // headers: {
+            //        Authorization: token,
+            //        'SignalRConnectionId': '8ce05f33-570c-4b0c-9abd-b2252d3fcc08',
+            //        'Accept': 'application/json',
+            //        'Content-Type': 'application/json'
+            //    },
+            headers: headers,
+            method: 'POST',
+            data: {
+                    id: datas.id, 
+                    organizationType: datas.organizationType, 
+                    description: datas.description, 
+                    reportingFrequency: datas.reportingFrequency, 
+                    details: datas.details
+                },
+        });
+    }
+}
---
+++ 12. ipdc-main-project_new_post_method_adding_header_14th_Feb_2020_4pmpm/src/views/PlanAndReports.vue
@@ -0,0 +1,253 @@
+<template>
+  <v-ons-page>
+    <div class="plan-and-reports">
+      <!-- <h3>Plan &amp; Repors</h3> -->
+        <div class="center search" style="text-align: right;">
+            <div id="search-input" placeholder="Search">
+                <input type="search" class="hh search-input search-input--material" placeholder="Search">
+            </div>
+        </div>
+        
+        <button class="waves-effect waves-light btn" @click="createNew">Create</button>
+        <div style="margin: 5px 0">
+            <div style="margin: 5px 0">
+                <button class="waves-effect waves-light btn" @click="searchAll">Search All</button>
+            </div>
+            <div style="margin: 5px 0">
+                <button class="waves-effect waves-light btn" @click="searchOrganization">Search Organization</button>
+            </div>
+            <div style="margin: 5px 0">
+                <button class="waves-effect waves-light btn" @click="organization">Organization</button>
+            </div>
+            <div style="margin: 5px 0">
+                <button class="waves-effect waves-light btn" @click="organizationUser">Organization User</button>
+            </div>
+            <div style="margin: 5px 0">
+                <button class="waves-effect waves-light btn" @click="getPlanById">Get Plan By Id</button>
+            </div>
+            <div style="margin: 5px 0">
+                <button class="waves-effect waves-light btn" @click="testingSignalrConnection">Signalr Test</button>
+            </div>
+        </div>
+
+        <div class="report-unit-sec">
+            <div class="row plan-and-report-row">
+                <v-ons-card class="card-radius plan-card">
+                    <div>
+                        <router-link to='/planandreports/report_landing_page'>
+                            <div class="content">
+                                <p class="report-card">2019_Dec-2019_Dec_Unit_Traning Unit</p>
+                            </div>
+                        </router-link>
+                    </div>
+                    <div>
+                        <router-link to='/planandreports/report_landing_page'>
+                            <div class="content">
+                                <p class="report-card">2019_Dec-2019_Dec_Unit_Traning Unit</p>
+                            </div>
+                        </router-link>
+                    </div>
+                </v-ons-card>
+            </div>
+        </div>
+    </div>
+  </v-ons-page>
+</template>
+
+<script>
+import UnitPlanReportService from '../services/UnitPlanReportService'
+const unitPlanReportService = new UnitPlanReportService();
+
+export default {
+  components: {
+  },
+
+  data () {
+    return {
+        appName: process.env.VUE_APP_TITLE
+    }
+  },
+  created() {
+    // let componentName = {'name': 'Plan & Reports'};
+    // localStorage.setItem('ipdc_current_component_name', JSON.stringify(componentName));
+  },
+  methods:{
+    createNew() {/*------------------------------ Create New Default button ------------------------------*/
+      console.log('Create button clicked');
+      unitPlanReportService
+        .getAllPosts()
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err =>{
+            console.error(err);
+        });
+    },
+    searchAll() {/*------------------------------ Search All button ------------------------------*/
+      console.log('Search All');
+      unitPlanReportService
+        .getSearchAllPage()
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err =>{
+            console.error(err);
+        });
+    },
+    searchOrganization() {/*------------------------------ Search Organization button ------------------------------*/
+      console.log('Search Organization');
+      unitPlanReportService
+        .getSearchOrganization()
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err =>{
+            console.error(err);
+        });
+    },
+    organization() {/*------------------------------ Get Organization button ------------------------------*/
+      console.log('Get Organization');
+      unitPlanReportService
+        .getOrganization()
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err =>{
+            console.error(err);
+        });
+    },
+    organizationUser() {/*------------------------------ Get Organization User button ------------------------------*/
+      console.log('Get Organization User');
+      unitPlanReportService
+        .getOrganizationUser()
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err =>{
+            console.error(err);
+        });
+    },
+    getPlanById() {/*------------------------------ Get Plan by Plan Id ------------------------------*/
+      console.log('Get Plan By id');
+      unitPlanReportService
+        .getPlanById()
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err =>{
+            console.error(err);
+        });
+    },
+    testingSignalrConnection() {
+      console.log('Signalr btn clicked');
+      /*var connection = new signalR.HubConnection()
+        .withUrl("http://localhost:50009//signalr")
+        // .configureLogging(signalR.LogLevel.Information)
+        // .build();
+      console.log('Connection : ' + connection);
+      console.log(connection);
+      console.log(connection.id);*/
+
+      /*const connection = new HubConnectionBuilder()
+      .withUrl('http://localhost:50009/signalr', {useDefaultPath: false})
+      .configureLogging(LogLevel.Information)
+      .build(); 
+      console.log(connection);
+
+
+    //     this.startedPromise = connection.start().catch(err => {
+    //         console.error('Failed to connect with hub', err)
+    //         return new Promise((resolve, reject) => 
+    //         setTimeout(() => start().then(resolve).catch(reject), 5000))
+    //     })
+
+    //     connection.onclose(() => start())*/
+
+        const OrganizationReference = {
+                        organization:
+                            {
+                                id: 25,
+                                organizationType: "1",
+                                description: "WMN Unit",
+                                reportingFrequency: "1",
+                                details: null
+                            },
+                        year: 2019,
+                        reportingTerm: 10,
+                        reportingFrequency: 1
+                    };
+        console.log(OrganizationReference.organization);
+        console.log(JSON.stringify(OrganizationReference.organization));
+        // console.log(orgRef.organizationReference);
+        console.log(OrganizationReference.year);
+        console.log(OrganizationReference.reportingTerm);
+        console.log(OrganizationReference.reportingFrequency);
+
+        const link = 'http://localhost:50009//reporting/v1/unit/plan/create2?year=' 
+                        + OrganizationReference.year + '&reportingTerm=' + OrganizationReference.reportingTerm + '&reportingFrequency=' + OrganizationReference.reportingFrequency;
+        // console.log(link);
+
+        unitPlanReportService
+            .createPlan2(link, OrganizationReference.organization)
+            .then(res => {
+                console.log('this is the res : ');
+                console.log(res.data);
+            })
+            .catch(err =>{
+                console.log('this is the error : ');
+                console.error(err);
+            });
+    }
+  },
+  watch: {
+  },
+};
+</script>
+
+<style scoped>
+.plan-and-reports {
+    padding: 0 4px;
+}
+.planandreport-title {
+    font-size: 26px;
+    margin: 0px;
+} 
+/* .title {
+    text-align: left;
+    font-size: 26px;
+    margin: 10px;
+    position: absolute;
+    top: 0;
+    background: #EE6E73;
+    color: #fff;
+    min-width: 35%;
+} */
+.search {
+    text-align: right;
+}
+.search ons-search-input {
+    margin-right: 10px;
+}
+.search input.search-input.search-input--material {
+    height: 28px !important;
+    background-size: 13px;
+    background-position: 8px center;
+    padding: 0 8px 0 28px !important;
+    width: auto !important;
+}
+.plan-and-report-row {
+    margin: 0;
+}
+.card-radius.plan-card {
+    margin: 8px 0;
+    width: 100%;
+}
+.card-radius.plan-card .report-card {
+    border-bottom: 1px solid gainsboro;
+    padding-bottom: 5px;
+    margin-top: 0;
+    margin-bottom: 5px;
+    font-size: 16px;
+    color: rgba(0, 0, 0, .54);
+}
+</style>
